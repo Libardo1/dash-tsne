ad8f84ac3c1803079d94408c354a069734dd7720
Xing Han Lu <xhlperso@gmail.com>
2018-05-09 13:51:38 -0400

    Fixed problem with global variable
    Removed global variables, replaced with invisible Divs that store JSON serialized dataframes

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import pandas as pd
 import plotly.graph_objs as go
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-import json
 
 app = dash.Dash(__name__)
 server = app.server
@@ -24,22 +23,6 @@ if 'DYNO' in os.environ:
     app.scripts.append_script({
         'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'
     })
-
-# Initialize the global variables to null
-data_df, label_df = None, None
-
-color_list = [
-    '#FAA613',
-    '#F44708',
-    '#A10702',
-    '#550527',
-    '#688E26',
-    '#393D3F',
-    '#D5BBB1',
-    '#9CC4B2',
-    '#C98CA7',
-    '#E76D83'
-]
 
 # Generate the default scatter plot
 tsne_df = pd.read_csv("data/tsne_3d.csv", index_col=0)
@@ -56,7 +39,6 @@ for idx, val in tsne_df.groupby(tsne_df.index):
         z=val['z'],
         mode='markers',
         marker=dict(
-            color=color_list[idx],
             size=2.5,
             symbol='circle-dot'
         )
@@ -110,18 +92,17 @@ tsne_layout = go.Layout(
 # App
 app.layout = html.Div([
     # In-browser storage of global variables
-    html.Div([
-        html.Div(
-            id="data-df-and-message",
-            style={'display': 'none'}
-        ),
+    html.Div(
+        id="data-df-and-message",
+        style={'display': 'none'}
+    ),
 
-        html.Div(
-            id="label-df-and-message",
-            style={'display': 'none'}
-        )
-    ]),
+    html.Div(
+        id="label-df-and-message",
+        style={'display': 'none'}
+    ),
 
+    # Main app
     html.Div([
         html.H2(
             't-SNE Explorer',
@@ -140,7 +121,9 @@ app.layout = html.Div([
                 'float': 'right'
             }
         )
-    ]),
+    ],
+        className="row"
+    ),
 
     html.Div([
         html.Div([
@@ -261,9 +244,6 @@ app.layout = html.Div([
         'width': '90%',
         'max-width': 'none',
         'font-size': '1.5rem'
-        # 'border-radius': '5px',
-        # 'box-shadow': 'rgb(240, 240, 240) 5px 5px 5px 0px',
-        # 'border': 'thin solid rgb(240, 240, 240)'
     }
 )
 
@@ -297,11 +277,11 @@ def parse_content(contents, filename):
     return df, f'{filename} successfully processed.'
 
 
+# Uploaded data --> Hidden Data Div
 @app.callback(Output('data-df-and-message', 'children'),
               [Input('upload-data', 'contents'),
                Input('upload-data', 'filename')])
 def parse_data(contents, filename):
-
     data_df, message = parse_content(contents, filename)
 
     if data_df is None:
@@ -313,11 +293,11 @@ def parse_data(contents, filename):
     return [data_df.to_json(orient="split"), message]
 
 
+# Uploaded labels --> Hidden Label div
 @app.callback(Output('label-df-and-message', 'children'),
               [Input('upload-label', 'contents'),
                Input('upload-label', 'filename')])
 def parse_label(contents, filename):
-
     label_df, message = parse_content(contents, filename)
 
     if label_df is None:
@@ -329,18 +309,21 @@ def parse_label(contents, filename):
     return [label_df.to_json(orient="split"), message]
 
 
+# Hidden Data Div --> Display upload status message (Data)
 @app.callback(Output('upload-data-message', 'children'),
               [Input('data-df-and-message', 'children')])
 def output_upload_status_data(data):
     return data[1]
 
 
+# Hidden Label Div --> Display upload status message (Labels)
 @app.callback(Output('upload-label-message', 'children'),
               [Input('label-df-and-message', 'children')])
 def output_upload_status_label(data):
     return data[1]
 
 
+# Button Click --> Update graph with states
 @app.callback(Output('plot-div', 'children'),
               [Input('tsne-train-button', 'n_clicks')],
               [State('perplexity-state', 'value'),
@@ -356,139 +339,115 @@ def update_graph(n_clicks, perplexity, n_iter, learning_rate, pca_dim, data_div,
     # Fix for startup POST
     if n_clicks <= 0 or data_div is None or label_div is None:
         global data
-        return [
-            # Data about the graph
-            html.Div(
-                id="kl-divergence",
-                style={'display': 'none'}
-            ),
+        kl_divergence, end_time = None, None
 
-            html.Div(
-                id="end-time",
-                style={'display': 'none'}
-            ),
+    else:
+        # Extract the data dataframe and the labels dataframe from the divs. they are both the first child of the div,
+        # and are serialized in json
+        data_df = pd.read_json(data_div[0], orient="split")
+        label_df = pd.read_json(label_div[0], orient="split")
 
-            # The graph
-            dcc.Graph(
-                id='tsne-3d-plot',
-                figure={
-                    'data': data,
-                    'layout': tsne_layout
-                },
-                style={
-                    'height': '80vh',
-                },
+        # Fix the range of possible values
+        if n_iter > 1000:
+            n_iter = 1000
+        elif n_iter < 10:
+            n_iter = 10
+
+        if perplexity > 50:
+            perplexity = 50
+        elif perplexity < 5:
+            perplexity = 5
+
+        if learning_rate > 1000:
+            learning_rate = 1000
+        elif learning_rate < 10:
+            learning_rate = 10
+
+        if pca_dim > data_df.shape[1]:  # We limit the pca_dim to the dimensionality of the dataset
+            pca_dim = data_df.shape[1]
+        elif pca_dim < 3:
+            pca_dim = 3
+
+        # Start timer
+        start_time = time.time()
+
+        # Apply PCA on the data first
+        pca = PCA(n_components=pca_dim)
+        data_pca = pca.fit_transform(data_df)
+
+        # Then, apply t-SNE with the input parameters
+        tsne = TSNE(n_components=3,
+                    perplexity=perplexity,
+                    learning_rate=learning_rate,
+                    n_iter=n_iter)
+
+        data_tsne = tsne.fit_transform(data_pca)
+        kl_divergence = tsne.kl_divergence_
+
+        # Combine the reduced t-sne data with its label
+        tsne_data_df = pd.DataFrame(data_tsne, columns=['x', 'y', 'z'])
+
+        label_df.columns = ['label']
+
+        combined_df = tsne_data_df.join(label_df)
+
+        data = []
+
+        # Group by the values of the label
+        for idx, val in combined_df.groupby('label'):
+            scatter = go.Scatter3d(
+                name=idx,
+                x=val['x'],
+                y=val['y'],
+                z=val['z'],
+                mode='markers',
+                marker=dict(
+                    size=2.5,
+                    symbol='circle-dot'
+                )
             )
-        ]
+            data.append(scatter)
 
-    # Extract the data dataframe and the labels dataframe from the divs. they are both the first child of the div, and
-    # are serialized in json
-    data_df = pd.read_json(data_div[0], orient="split")
-    label_df = pd.read_json(label_div[0], orient="split")
-
-    # Fix the range of possible values
-    if n_iter > 1000:
-        n_iter = 1000
-    elif n_iter < 10:
-        n_iter = 10
-
-    if perplexity > 50:
-        perplexity = 50
-    elif perplexity < 5:
-        perplexity = 5
-
-    if learning_rate > 1000:
-        learning_rate = 1000
-    elif learning_rate < 10:
-        learning_rate = 10
-
-    if pca_dim > data_df.shape[1]:  # We limit the pca_dim to the dimensionality of the dataset
-        pca_dim = data_df.shape[1]
-    elif pca_dim < 3:
-        pca_dim = 3
-
-    # Start timer
-    start_time = time.time()
-
-    # Apply PCA on the data first
-    pca = PCA(n_components=pca_dim)
-    data_pca = pca.fit_transform(data_df)
-
-    # Then, apply t-SNE with the input parameters
-    tsne = TSNE(n_components=3,
-                perplexity=perplexity,
-                learning_rate=learning_rate,
-                n_iter=n_iter)
-
-    data_tsne = tsne.fit_transform(data_pca)
-    kl_divergence = tsne.kl_divergence_
-
-    # Combine the reduced t-sne data with its label
-    tsne_data_df = pd.DataFrame(data_tsne, columns=['x', 'y', 'z'])
-
-    label_df.columns = ['label']
-
-    combined_df = tsne_data_df.join(label_df)
-
-    data = []
-
-    color_idx_counter = 0
-
-    # Group by the values of the label
-    for idx, val in combined_df.groupby('label'):
-        scatter = go.Scatter3d(
-            name=idx,
-            x=val['x'],
-            y=val['y'],
-            z=val['z'],
-            mode='markers',
-            marker=dict(
-                color=color_list[color_idx_counter],
-                size=2.5,
-                symbol='circle-dot'
-            )
-        )
-        data.append(scatter)
-
-        color_idx_counter += 1
-
-    end_time = time.time() - start_time
+        end_time = time.time() - start_time
 
     return [
-            # Data about the graph
-            html.Div([
-                kl_divergence
-            ],
-                id="kl-divergence",
-                style={'display': 'none'}
-            ),
+        # Data about the graph
+        html.Div([
+            kl_divergence
+        ],
+            id="kl-divergence",
+            style={'display': 'none'}
+        ),
 
-            html.Div([
-                end_time
-            ],
-                id="end-time",
-                style={'display': 'none'}
-            ),
+        html.Div([
+            end_time
+        ],
+            id="end-time",
+            style={'display': 'none'}
+        ),
 
-            # The graph
-            dcc.Graph(
-                id='tsne-3d-plot',
-                figure={
-                    'data': data,
-                    'layout': tsne_layout
-                },
-                style={
-                    'height': '80vh',
-                },
-            )
-        ]
+        # The graph
+        dcc.Graph(
+            id='tsne-3d-plot',
+            figure={
+                'data': data,
+                'layout': tsne_layout
+            },
+            style={
+                'height': '80vh',
+            },
+        )
+    ]
 
 
+# Updated graph --> Training status message
 @app.callback(Output('training-status-message', 'children'),
               [Input('end-time', 'children'),
                Input('kl-divergence', 'children')])
 def update_training_info(end_time, kl_divergence):
-    if end_time is not None and kl_divergence is not None:
+    if end_time is None or kl_divergence is None or end_time[0] is None or kl_divergence[0] is None:
+        return None
+    else:
         end_time = end_time[0]
         kl_divergence = kl_divergence[0]
 
